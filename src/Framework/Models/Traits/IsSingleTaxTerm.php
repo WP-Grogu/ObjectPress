4a073fa8dfeaa39b05c1ba5ddc2b39e1057ff8c8
thomas <thomas@148.fr>
2020-05-27 23:58:02 +0200

    feat: dding TaxonomySingleTerm lib example

---
+++ src/Framework/Models/Traits/IsSingleTaxTerm.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Transform 'website-space' metabox to a TaxonomySingleTerm one.
+ * This way, we can force a selection and set a radio button (only one term possible)
+ */
+// $theme->on('init', function () {
+//     $wa_tax_box = new App\Classes\TaxonomySingleTerm('website-area');
+
+//     $wa_tax_box->set('metabox_title', __('Espace du site', '148-theme'));
+//     $wa_tax_box->set('force_selection', true);
+//     $wa_tax_box->set('allow_new_terms', false);
+//     $wa_tax_box->set('default', 'debitor');
+// }, 13);
