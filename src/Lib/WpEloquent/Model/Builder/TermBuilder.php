98bd03fdd13e7be4b2ca95eaf9271d19af1eb82f
Thomas <themagnetiiktbk@gmail.com>
2021-08-10 14:51:35 +0200

    :bug: Alter orderByMeta() to support natural sorting

--- src/Lib/WpEloquent/Model/Builder/TermBuilder.php
+++ src/Lib/WpEloquent/Model/Builder/TermBuilder.php
@@ -9,7 +9,7 @@ use OP\Lib\WpEloquent\Connection;
  * Class TermBuilder
  *
  * @package ObjectPress
- * @author Thomas Georgel <thom.georgel@gmail.com>
+ * @author  Thomas Georgel <thom.georgel@gmail.com>
  */
 class TermBuilder extends Builder
 {
@@ -43,10 +43,8 @@ class TermBuilder extends Builder
         $db     = Connection::instance();
         $prefix = $db->getPdo()->prefix();
 
-        return $this->select([
-                    $prefix.'terms.*',
-                    $db->raw("(select meta_value from {$prefix}termmeta where {$prefix}termmeta.meta_key = '{$meta_key}' and {$prefix}terms.term_id = {$prefix}termmeta.term_id limit 1) as meta_ordering")
-                ])
+        return $this->select([$prefix.'terms.*', $db->raw("(select meta_value from {$prefix}termmeta where {$prefix}termmeta.meta_key = '{$meta_key}' and {$prefix}terms.term_id = {$prefix}termmeta.term_id limit 1) as meta_ordering")])
+                ->orderByRaw('LENGTH(meta_ordering)', 'ASC') # alphanum support, avoid this kind of sort : 1, 10, 11, 7, 8
                 ->orderBy('meta_ordering', $order);
     }
 }
