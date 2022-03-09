29d50799aec6af85321d7c1128aeed9f7df80b5a
Thomas <themagnetiiktbk@gmail.com>
2022-03-09 19:33:11 +0100

    :zap: Minor change WpCli Command

--- src/Framework/Contracts/WpCliCommand.php
+++ src/Framework/Contracts/WpCliCommand.php
@@ -4,5 +4,11 @@ namespace OP\Framework\Contracts;
 
 interface WpCliCommand
 {
+    /**
+     * The action done by this command.
+     *
+     * @param array $args The conmmand arguments as returned by WpCLI.
+     * @return void
+     */
     public function execute(array $args);
 }
